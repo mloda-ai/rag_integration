9094fd420584e1b447033245aee88678c9c2a641
Tom Kaltofen <tomkaltofen@gmail.com>
2026-03-22 11:32:22 +0100

    chore: remove template boilerplate and artifacts

--- rag_integration/compute_frameworks/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Compute frameworks namespace package."""
--- rag_integration/extenders/my_plugin/__init__.py
+++
@@ -1,5 +0,0 @@
-"""My Plugin Extender package."""
-
-from rag_integration.extenders.my_plugin.my_extender import MyExtender
-
-__all__ = ["MyExtender"]
